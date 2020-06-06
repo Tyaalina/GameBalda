4ac03a018debb6f6691d4817eb583cc26148ecfb
Turitsyna Alina <alca_cat_1999@rambler.ru>
2020-06-06 11:32:49 +0400

    Добавлены события поля

--- GameBalda/src/main/java/MyEvents/GameListener.java
+++ GameBalda/src/main/java/MyEvents/GameListener.java
@@ -6,4 +6,7 @@ public interface GameListener extends EventListener {
      void gameFinished(GameEvent e);
      void playerExchanged(GameEvent e);
      void deadHeat(GameEvent e);
+
+     void wordNotFromDictionary(GameEvent e);
+     void wordIsAlreadyUsedOnField(GameEvent e);
 }
